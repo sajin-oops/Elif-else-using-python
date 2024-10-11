df3a0d9fb46c528c6a34c82810e629017a86aa4e
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-11 16:54:53 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,16 @@
+mark = int(input("Enter your mark: "))
+if mark >= 90:
+    print("Grade A")
+elif mark >= 80:
+    print("Grade B")
+elif mark >=50:
+    print("Grade C")
+else:
+    print("Welcome to the real world")
+'''
+O/P 
+
+Enter your mark:  99
+Grade A
+
+'''
